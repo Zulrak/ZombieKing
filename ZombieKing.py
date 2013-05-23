b801aa3a4e4976dccc3b91c7816af7df6f8ee247
Jordan Cooper <200199215@student.georgianc.on.ca>
2013-05-23 12:39:33 -0400

    Zombie King Version 1.3
    Created the Catacombs option for the game. the player can now access
    catacombs / down/ take or leave the gear/ Win or lose the game

--- ZombieKing.py
+++ ZombieKing.py
@@ -40,8 +40,8 @@ def decision1():
         print ('You step back into the church. You hear noises from all around you. The way you see it, there are two options...')
         time.sleep(2)
         print('You can search the church, or the catacombs below the church to search for your companion')
-        print('')
         time.sleep(2)
+        print('')
         print('Please enter your choice (church or catacombs): ')
         choice = raw_input()
 
@@ -49,7 +49,7 @@ def decision1():
             churchDecision2()
         else:
             catacombsDecision2()
-
+#Church______________________________________________________________________________________________
 def churchDecision2():
     choice =''
     while choice != 'left' and choice != 'right':
@@ -58,6 +58,7 @@ def churchDecision2():
         print ('You Continue to walk until you reach the end of the hallway, you can go left into the next room')
         print ('Or you can go right, and follow the adjoining hallway')
         time.sleep(3)
+        print('')
         print ('Please enter your choice left or right: ')
         choice = raw_input()
 
@@ -65,7 +66,7 @@ def churchDecision2():
             churchDecision3Left()
         else:
             churchDecision3Right()
-#LEFT decision2 _________________________________________________________________________________________           
+#LEFT Church _________________________________________________________________________________________           
 def churchDecision3Left():
     choice =''
     while choice != 'MG' and choice != 'HG':
@@ -74,16 +75,16 @@ def churchDecision3Left():
         print ('You find a machine gun and ammo in a small crate in the room.')
         print ("You can take the machine gun but you can't carry your hand gun and the machine gun.")
         time.sleep(3)
+        print('')
         print ('Please enter your choice MG (for take machine gun) or HG (for keep hand gun): ')
         choice = raw_input()
     
         if choice == "MG":
-            decision4AA()
+            churchDecision4MG()
         else:
-            decision4AB()
+            churchDecision4HG()
 
-
-def decision4AA():
+def churchDecision4MG():
     print ('You take the Machine Gun and continue into the next room')  
     print ('Suddenly a horde of zombies pop out and attack you!')
     time.sleep(2)
@@ -100,7 +101,7 @@ def decision4AA():
     print ('')
     print ('')
     
-def decision4AB():
+def churchDecision4HG():
     print ('You leave the Machine Gun and continue into the next room')   
     print ('Suddenly a horde of zombies pop out and attack you!')
     time.sleep(2)
@@ -112,6 +113,7 @@ def decision4AB():
     time.sleep(3)
     print ('')
     print ('')
+    
 #RIGHT_________________________________________________________________________________________________
   
 def churchDecision3Right():
@@ -124,6 +126,7 @@ def churchDecision3Right():
         print ('As a skilled zombie killer you can sneak past these zombies without being detected...usually.')
         print ("Or you can put those zombie killing skills to use and fight them like a man!.")
         time.sleep(3)
+        print('')
         print ('Please enter your choice sneak or fight: ')
         choice = raw_input()
         
@@ -134,48 +137,155 @@ def churchDecision3Right():
         
 def churchDecision4Sneak():
     
-        print ('You continue to sneak down the hallway...')
-        time.sleep(1)
-        print ('As you slowly move past the horde of zombies, making sure to not to make a sound...')
-        time.sleep(2)
-        print ('You accidently step on a rotten floor board!')
-        time.sleep(2)
-        print ("The zombies suddenly become aware of your presence!.")
-        time.sleep(2)
-        print ("The zombies charge at you! They claw, bite and chase you as you start to run!.")
-        time.sleep(2)
-        print ("While running away from the zombies you are bitten!")
-        time.sleep(2)
-        print ('The virus begins to spread through your viens.')
-        time.sleep(1)
-        print ("You try to fight it but their is no hope.")    
-        time.sleep(1)
-        print ("You have turned into a zombie! You have died!")    
+    print ('You continue to sneak down the hallway...')
+    time.sleep(1)
+    print ('As you slowly move past the horde of zombies, making sure to not to make a sound...')
+    time.sleep(2)
+    print ('You accidently step on a rotten floor board!')
+    time.sleep(2)
+    print ("The zombies suddenly become aware of your presence!.")
+    time.sleep(2)
+    print ("The zombies charge at you! They claw, bite and chase you as you start to run!.")
+    time.sleep(2)
+    print ("While running away from the zombies you are bitten!")
+    time.sleep(2)
+    print ('The virus begins to spread through your viens.')
+    time.sleep(1)
+    print ("You try to fight it but their is no hope.")    
+    time.sleep(1)
+    print ("You have turned into a zombie! You have died!")    
         
 def churchDecision4Fight():
-    
-        print ('You continue to walk down the hallway...')
-        time.sleep(1)
-        print ('You pick up a rotten floor board and charge at the zombies!')
-        time.sleep(2)
-        print ('Even with all of your zombie killing skill you are no match for the zombie horde!')
-        time.sleep(2)
-        print ("While you savagely strike at the zombies you are bitten!")
-        time.sleep(2)
-        print ('The virus begins to spread through your viens.')
-        time.sleep(1)
-        print ("You try to fight it but their is no hope.")    
-        time.sleep(1)
-        print ("You have turned into a zombie! You have died!")    
         
-#End of the church options________________________________________________________________________________
+    print ('You continue to walk down the hallway...')
+    time.sleep(1)
+    print ('You pick up a rotten floor board and charge at the zombies!')
+    time.sleep(2)
+    print ('Even with all of your zombie killing skill you are no match for the zombie horde!')
+    time.sleep(2)
+    print ("While you savagely strike at the zombies you are bitten!")
+    time.sleep(2)
+    print ('The virus begins to spread through your viens.')
+    time.sleep(1)
+    print ("You try to fight it but their is no hope.")    
+    time.sleep(1)
+    print ("You have turned into a zombie! You have died!")    
         
-
+#Catacombs________________________________________________________________________________
+        
 def catacombsDecision2():
-    print ('test Choice 2 Chosen')
-   
+    choice =''
+    while choice != 'down' and choice != 'forward':
+        print ('You begin to search the church...')
+        time.sleep(2)
+        print ('You find the passageway to the catacombs under the alter!')
+        time.sleep(2)
+        print ('You walk down the crumbling spiral staircase, with each step you feel colder and colder...')
+        time.sleep(2)
+        print ('When you reach the bottom of the staircase you realize there is a ladder that could lead to a second level of the catacombs')
+        time.sleep(2)
+        print ('You can go down the ladder or continue to search through the catacombs.')
+        time.sleep(3)
+        print('')
+        print ('Please enter your choice down or forward: ')
+        choice = raw_input()
+
+        if choice == "down":
+            catacombsDecision3Down()
+        else:
+            catacombsDecision3Forward()
+#Down________________________________________________________________________
+def catacombsDecision3Down():
+    choice =''
+    while choice != 'take' and choice != 'leave':
+        print ('You climb down the ladder and start to walk down the new hallway')
+        time.sleep(2)
+        print ('The hall is filled with the corpses of the dead,')
+        time.sleep(2)
+        print ('they must be the remains of other survivors who have come here searching for supplies.')
+        time.sleep(2)
+        print ('You notice the corpse of a prison gaurd. You can take his tatical supplies, and riot gear')
+        print ("but you won't be able to carry all of this gear and your medical pack.")
+        time.sleep(2)
+        print ('You will have to make a difficult choice, Take the gear or keep your medical pack...')
+        time.sleep(3)
+        print('')
+        print ("Please enter your choice 'take' (the tatical supplies) or 'leave' (the tatical supplies): ")
+        choice = raw_input()
+
+        if choice == "take":
+            catacombsDecision4Take()
+        else:
+            catacombsDecision4Leave()
+
+def catacombsDecision4Take():
+    
+    time.sleep(2)
+    print ("You put on the dead guard's riot gear and pick up his supplies")
+    time.sleep(2)
+    print ('You continue to walk down the hall until you reach the end of the hall')
+    time.sleep(2)
+    print ('At the end of the hall you see a large cave, upon closer inspection you see your companion!')
+    time.sleep(2)
+    print ('She is being guarded by the zombie king and his horde of zombies!')
+    time.sleep(2)
+    print ('You charge in and attack the zombie king!')
+    time.sleep(2)
+    print ('The zombies try to bite you but your are protected by your armor!')
+    time.sleep(2)
+    print ('With one steady shot with your pistol you shoot the zombie king in the head!')
+    time.sleep(2)
+    print ('You finish off the rest of the zombies with your tatical knife and rescue your companion!')
+    time.sleep(2)
+    print ('Congradulations you have successfully rescued your companion and defeated the zombie king!')
+
+def catacombsDecision4Leave():
+    
+    time.sleep(2)
+    print ('You continue to walk down the hall until you reach the end of the hall')
+    time.sleep(2)
+    print ('At the end of the hall you see a large cave, upon closer inspection you see your companion!')
+    time.sleep(2)
+    print ('She is being guarded by the zombie king and his horde of zombies!')
+    time.sleep(2)
+    print ('You charge in and attack the zombie king!')
+    time.sleep(2)
+    print ('The zombie king thorws his sword at you!')
+    time.sleep(2)
+    print ('You move to dodge the blade, the blade cuts your neck and opens up an artery')
+    time.sleep(2)
+    print ('You try to bandage your wound with your medical kit but there is no time!')
+    time.sleep(2)
+    print ('You try to untie your companion')
+    time.sleep(2)
+    print ('The horde blocks your path!')
+    time.sleep(1)
+    print ("You try to run but it's too late!")
+    time.sleep(1)
+    print ('You have lost too much blood. You start to black out...')
+    time.sleep(2)
+    print ('You lost too much blood to survive. The horde of zombies eats your corpse!')
+    print ('You have died!!!')
 
 
+#Forward_________________________________________________________________________________________
+
+def catacombsDecision3Forward():
+    choice =''
+    while choice != 'down' and choice != 'forward':
+        print ('You begin to search the church...')
+        time.sleep(2)
+        print ('You find the passageway into the catacombs under the alter!')
+        time.sleep(3)
+        print('')
+        print ('Please enter your choice down or forward: ')
+        choice = raw_input()
+
+        if choice == "down":
+            catacombsDecision3Down()
+        else:
+            catacombsDecision3Forward()
+#End of game_________________________________________________________________________________________          
 def main():    
     
     playAgain = 'yes'
